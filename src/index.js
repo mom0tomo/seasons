9c45e7ce4b32809801cb7931bf9e10c56e2a4828
mom0tomo <momo76w@gmail.com>
2019-01-02 21:45:33 +0900

    Excersize: class base components

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
 
-class App extends React.Component {
-	state = { lat: null, errorMessage: '' };
 
-	componentDidMount() {
-		window.navigator.geolocation.getCurrentPosition(
-			(position) => this.setState({ lat: position.coords.latitude }),
-			(err) => this.setState({ errorMessage: err.message })
-		);
-	}
-
-	renderContent() {
-		if (this.state.errorMessage && !this.state.lat) {
-			return <div>Error: {this.state.errorMessage}</div>;
-		}
-
-		if (!this.state.errorMessage && this.state.lat) {
-			return <SeasonDisplay lat={this.state.lat} />;
-		}
-
-		return <Spinner mesage="please accept location request" />;
-	}
-
+class UserForm extends React.Component {
 	render() {
-		return <div className="border red">{this.renderContent()}</div>;
-	}
+		return (
+			<form>
+				<label> Enter a username: </label>
+				<input />
+			</form>
+		);
+	};
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<UserForm />, document.querySelector('#root'));
